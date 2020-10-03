89a9e13c74963044a2c2f083533dba372bcd3edd
Aadi Manchekar <the.unbeatable.master2002@gmail.com>
2020-10-03 21:06:56 +0530

    Dynamically updated DATE

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
        <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2020</p>
+                    <p>Copyright &copy; Your Website <span id="root"></span></p>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
@@ -18,5 +18,14 @@
     <script src="js/bootstrap.min.js"></script>
 
 </body>
+
+<script>
+    let container = document.querySelector("#root");
+
+    let date = new Date();
+    
+    container.innerText = date.getFullYear();
+
+</script>
 
 </html>
