a5852b17b499cbae98541a9d7069f04bd066319e
Aadi Manchekar <the.unbeatable.master2002@gmail.com>
2020-10-04 23:51:44 +0530

    Fixed Add category error

--- admin/categories.php
+++ admin/categories.php
@@ -21,12 +21,11 @@
                             <?php
                                 if(isset($_POST['Submit'])){
                                     $cat_title = $_POST['cat_title'];
-                                    if($cat_title = "" || empty($cat_title)){
+                                    if($cat_title == "" || empty($cat_title)){
                                         echo "This field should not be empty";
                                     }
                                     else {
-                                        $query = "INSERT INTO categories (cat_title)";
-                                        $query .= "VALUES('{$cat_title}') ";
+                                        $query = "INSERT INTO CATEGORIES(cat_title) VALUE('" . $cat_title ."')";
                                         $create_category_query = mysqli_query($connection,$query);
 
                                         if(!$create_category_query){
